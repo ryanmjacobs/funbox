c702d3a5b7357e434f95f9ec8e5b2cd960d99300
Ryan Jacobs <ryan.mjacobs@gmail.com>
2016-09-18 14:10:56 -0700

    bleh;

--- js/main.js
+++ js/main.js
@@ -1,19 +1,11 @@
-function buildVRElement(vrDisplay) {
-  var frameData = new VRFrameData();
-
-  // Call function indefinitely, to grab data
-  function onAnimationFrame () {
-    vrDisplay.requestAnimationFrame(onAnimationFrame);
-
-    vrDisplay.getFrameData(frameData);
-    console.log(frameData.pose.orientation);
-  }
-  vrDisplay.requestAnimationFrame(onAnimationFrame);
-}
-
 // Enumerate the VRDisplays
 navigator.getVRDisplays().then(function (displays) {
-    for (var i = 0; i < displays.length; ++i) {
-      buildVRElement(displays[i]);
-    }
+    setInterval(function() {
+        for (var i = 0; i < displays.length; i++) {
+            var display = displays[i];
+
+            var pose = display.getPose();
+            document.getElementById("data").innerHTML += JSON.stringify(pose);
+        }
+    }, 500);
 });
