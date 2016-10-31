a5975f49875755df0f9a955827a24093f2fe7b88
Ryan Jacobs <ryan.mjacobs@gmail.com>
2016-10-31 00:47:20 -0700

    wip...

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 // Enumerate the VRDisplays
 navigator.getVRDisplays().then(function (displays) {
     setInterval(function() {
-        for (var i = 0; i < displays.length; i++) {
-            var display = displays[i];
-
-            var pose = display.getPose();
-            document.getElementById("data").innerHTML = JSON.stringify(pose);
-        }
-    }, 500);
+        var display = displays[0];
+        var pose = display.getPose();
+    }, 100);
 });
+
+function set_text(obj) {
+    document.getElementById("data").innerHTML = "";
+    append_text(obj);
+}
+
+function append_text(obj) {
+    document.getElementById("data").innerHTML += JSON.stringify(obj, null, 2);
+}
