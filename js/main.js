fb118f6b54b504d03adb24d6c34367cd0e9b0660
Ryan Jacobs <ryan.mjacobs@gmail.com>
2016-10-31 01:24:26 -0700

    wip pose

--- js/main.js
+++ js/main.js
@@ -4,10 +4,22 @@ navigator.getVRDisplays().then(function (displays) {
     display.resetPose();
 
     setInterval(function() {
-        var pose = display.getPose();
+      //var pose = display.getPose();
+        var pose = display.getImmediatePose();
+
+        var graph = "\n\n";
+        for (var key in pose.orientation)
+            graph += bar(pose.orientation[key]) + "\n";
+
         set_text(pose);
-    }, 100);
+        append_text(graph);
+    }, 50);
 });
+
+function bar(x) {
+    var str = "**********";
+    return str.slice(0, Math.round(x*10));
+}
 
 function set_text(obj) {
     document.getElementById("data").innerHTML = "";
@@ -15,5 +27,8 @@ function set_text(obj) {
 }
 
 function append_text(obj) {
-    document.getElementById("data").innerHTML += JSON.stringify(obj, null, 2);
+    if (typeof obj == "string")
+        document.getElementById("data").innerHTML += obj;
+    else
+        document.getElementById("data").innerHTML += JSON.stringify(obj, null, 2);
 }
