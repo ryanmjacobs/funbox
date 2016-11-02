10519ae0079506fa81379fcf1aaf7236b34531a5
Ryan Jacobs <ryan.mjacobs@gmail.com>
2016-11-02 01:38:03 -0700

    more conversions

--- static/js/main.js
+++ static/js/main.js
@@ -19,23 +19,37 @@ navigator.getVRDisplays().then(function (displays) {
         };
 
         // convert to euler angles
-        var ysq = q.y * q.y;
-        var t0 = -2 * (ysq + q.z * q.z) + 1;
-        var t1 =  2 * (q.x * q.y - q.w * q.z);
-        var t2 = -2 * (q.x * q.z + q.w * q.y);
-        var t3 =  2 * (q.y * q.z - q.w * q.x);
-        var t4 = -2 * (q.x * q.x + ysq) + 1;
+        {
+            var ysq = q.y * q.y;
+            var t0 = -2 * (ysq + q.z * q.z) + 1;
+            var t1 =  2 * (q.x * q.y - q.w * q.z);
+            var t2 = -2 * (q.x * q.z + q.w * q.y);
+            var t3 =  2 * (q.y * q.z - q.w * q.x);
+            var t4 = -2 * (q.x * q.x + ysq) + 1;
 
-        t2 = t2 >  1 ?  1 : t2;
-        t2 = t2 < -1 ? -1 : t2;
+            t2 = t2 >  1 ?  1 : t2;
+            t2 = t2 < -1 ? -1 : t2;
 
-        var pitch = (Math.asin(t2)      * 180/Math.PI + 180).toFixed(0);
-        var roll  = (Math.atan2(t3, t4) * 180/Math.PI + 180).toFixed(0);
-        var yaw   = (Math.atan2(t1, t0) * 180/Math.PI + 180).toFixed(0);
+            var pitch = (Math.asin(t2)      * 180/Math.PI + 0).toFixed(0);
+            var roll  = (Math.atan2(t3, t4) * 180/Math.PI + 0).toFixed(0);
+            var yaw   = (Math.atan2(t1, t0) * 180/Math.PI + 0).toFixed(0);
 
-        console.log(pitch, roll, yaw);
-        reset_text();
-        append_text(pitch + " " + roll + " " + yaw + "\n");
+            reset_text();
+            append_text(pitch + " " + roll + " " + yaw + "\n");
+        }
+
+        // THREE.js method
+        {
+            var a = new THREE.Euler().fromArray(pose.orientation);
+            var b = [a.x, a.y, a.z];
+
+            for (var c in b) {
+                b[c] = (b[c] * 360/Math.PI + 90).toFixed(0);
+            }
+
+            append_text(b.join(" ") + "\n");
+            console.log(b);
+        }
 
         var graph = "\n\n";
         for (var key in pose.orientation)
