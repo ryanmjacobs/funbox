80f37d20fe5bc5b908e5be3da4d51c65981b7598
Ryan Jacobs <ryan.mjacobs@gmail.com>
2016-11-20 12:07:38 -0800

    it all works!! kind of...

--- firmware/src/main.ino
+++ firmware/src/main.ino
@@ -1,19 +1,25 @@
 #include <Servo.h>
 
-Servo servo;
+Servo sx, sy;
 
 void setup() {
-    servo.attach(9);
+    sx.attach(5);
+    sy.attach(6);
     Serial.begin(115200);
     Serial.println("Launching servo loop...");
 }
 
 void loop() {
+    int xy = 0;
+
     if (Serial.available() > 0) {
         byte rx = Serial.read();
-        servo.write(rx);
-
         Serial.print("rx -> ");
         Serial.println(rx, DEC);
+
+        if (xy = !xy)
+            sx.write(rx);
+        else
+            sy.write(rx);
     }
 }
--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,6 @@
 var ws = new WebSocket("ws://" + location.hostname + ":8888");
 
 navigator.getVRDisplays().then(function (displays) {
-    var cnt = 0;
     var last = null;
     var display = displays[0];
     display.resetPose();
@@ -10,61 +9,20 @@ navigator.getVRDisplays().then(function (displays) {
       //var pose = display.getPose();
         var pose = display.getImmediatePose();
 
-        // read quarternion
-        var q = {
-            w: pose.orientation[0],
-            x: pose.orientation[1],
-            y: pose.orientation[2],
-            z: pose.orientation[3]
-        };
-
-        // convert to euler angles
-        {
-            var ysq = q.y * q.y;
-            var t0 = -2 * (ysq + q.z * q.z) + 1;
-            var t1 =  2 * (q.x * q.y - q.w * q.z);
-            var t2 = -2 * (q.x * q.z + q.w * q.y);
-            var t3 =  2 * (q.y * q.z - q.w * q.x);
-            var t4 = -2 * (q.x * q.x + ysq) + 1;
-
-            t2 = t2 >  1 ?  1 : t2;
-            t2 = t2 < -1 ? -1 : t2;
-
-            var pitch = (Math.asin(t2)      * 180/Math.PI + 0).toFixed(0);
-            var roll  = (Math.atan2(t3, t4) * 180/Math.PI + 0).toFixed(0);
-            var yaw   = (Math.atan2(t1, t0) * 180/Math.PI + 0).toFixed(0);
-
-            reset_text();
-            append_text(pitch + " " + roll + " " + yaw + "\n");
-        }
-
         // THREE.js method
         var a = new THREE.Euler().fromArray(pose.orientation);
         var b = [a.x, a.y, a.z];
 
-        for (var c in b) {
+        // print out pitch, roll, yaw
+        for (var c in b)
             b[c] = (b[c] * 360/Math.PI + 90).toFixed(0);
-        }
 
+        set_pan(b[0], b[1]);
+
+        reset_text();
         append_text(b.join(" ") + "\n");
-        console.log(b);
-
-      //var graph = "\n\n";
-      //for (var key in pose.orientation)
-      //    graph += key + " " + bar(pose.orientation[key]) + "\n";
-
-        if (cnt++ % 100) {
-            var val = 180-b[1];
-
-            if (val != last)
-                set_pan(val);
-
-            last = val;
-        }
-
         append_text(pose);
-        append_text(graph);
-    }, 100);
+    }, 50);
 });
 
 function set_pan(x, y) {
